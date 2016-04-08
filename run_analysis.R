dae6df834149e7b0c0d308f6e5d5d9556d34dea9
Aaron Traub <actraub@Kaseys-Air.fios-router.home>
2016-04-07 20:37:51 -0400

    added grep and merge

--- run_analysis.R
+++ run_analysis.R
@@ -12,34 +12,50 @@ mergeset <- function(setup = FALSE) {
     # location of files
     trainset <- "./UCI HAR Dataset/train/X_train.txt"
     trainlabel <- "./UCI HAR Dataset/train/y_train.txt"
+    trainsub <- "./UCI HAR Dataset/train/subject_train.txt"
     
     testset <- "./UCI HAR Dataset/test/X_test.txt"
     testlabel <- "./UCI HAR Dataset/test/y_test.txt"
+    testsub <- "./UCI HAR Dataset/test/subject_test.txt"
     
     columheaders <- "./UCI HAR Dataset/features.txt"
     acts <- "./UCI HAR Dataset/activity_labels.txt"
     # labelvector <- c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTING", "STANDING", "LAYING")
     
-    
-    
     # load data
     test <- read.table(testset, header = FALSE)
     testA <- read.table(testlabel, header = FALSE)
+    testS <- read.table(testsub, header = FALSE)
     
     train <- read.table(trainset, header = FALSE)
     trainA <- read.table(trainlabel, header = FALSE)
+    trainS <- read.table(trainsub, header = FALSE)
     
     features <- read.table(columheaders, header = FALSE)
     activities <- read.table(acts, header = FALSE)
+    
     # merge the datasets
     newset <- rbind(test, train)
+    
     # add column names
     colnames(newset) <- features[,2]
+    
+    #subset colums 
+    newset <- subset(newset, select = grep("*mean*|*std*", names(newset)))
+    
     newacts <- rbind(testA, trainA)
     newset <- cbind(newset, newacts)
-    # rename(newset, activity1 = V1)
-    mutate(activities, activities = activities$V1 )
-    print(activities)
+    # add activity names
+    merged <- merge (newset, activities)
+    
+    newsubs <- rbind(testS, trainS)
+    newset <- cbind(newset, newsubs)
+    # newset <- dplyr::rename(newset, activity = V1)
+    
+## Add other colum names    
+    
+    head(merged)
+    # print(activities) 
     # head(newset)
     # str(newset)
     # dim(newset)
